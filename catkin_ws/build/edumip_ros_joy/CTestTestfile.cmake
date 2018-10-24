4da7f9308a64b9c737f8c284049cc5741e83f246
Ivliev <123ivliev123@mail.ru>
2018-10-24 12:21:50 +0300

    --all

--- catkin_ws/build/edumip_ros_joy/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ivliev/catkin_ws/src/edumip_ros_joy
-# Build directory: /home/ivliev/catkin_ws/build/edumip_ros_joy
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
